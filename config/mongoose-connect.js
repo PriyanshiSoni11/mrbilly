5fa7280b4cc3bc9c4d69d04e0dc25118db8aa092
PriyanshiATSamagra <priyanshi@samagragovernance.in>
2024-12-10 16:57:22 +0530

    mongoDB atls connection added;

--- config/mongoose-connect.js
+++ config/mongoose-connect.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose')
 const dbgr = require('debug')('development:mongoose-connetion')
 
-const dbUrl = `${process.env.MONGOOSE_CONNECT_URL}/mrbilly`;
+const dbUrl = `${process.env.MONGOOSE_CONNECT_URL}/mrbilly` || `${process.env.MONGOOSE_CONNECT_SERVER}/mrbilly`;
 
 mongoose
     .connect(dbUrl)
