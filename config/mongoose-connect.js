d8233793eb75d6a220789844371c38bf4f53f6b8
PriyanshiATSamagra <priyanshi@samagragovernance.in>
2024-12-10 18:12:54 +0530

    db connection fix

--- config/mongoose-connect.js
+++ config/mongoose-connect.js
@@ -1,17 +1,21 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const dbgr = require('debug')('development:mongoose-connection');
 
-const dbUrl =
-  process.env.MONGOOSE_CONNECT_URL
-    ? `${process.env.MONGOOSE_CONNECT_URL}/mrbilly`
-    : `${process.env.MONGOOSE_CONNECT_SERVER}/mrbilly`;
+const dbUrl = `${process.env.MONGOOSE_CONNECT_SERVER}`;
 
 mongoose
-  .connect(dbUrl)
+  .connect(dbUrl, { useNewUrlParser: true }) // Removed useUnifiedTopology
   .then(() => dbgr('Connected to MongoDB'))
-  .catch(err => dbgr('Error connecting to MongoDB:', err));
+  .catch(err => {
+    dbgr('Error connecting to MongoDB:', err);
+  });
+
+mongoose.connection.on('connected', () => dbgr('Mongoose default connection is open'));
+mongoose.connection.on('error', err => dbgr(`Mongoose default connection error: ${err}`));
+mongoose.connection.on('disconnected', () => dbgr('Mongoose default connection is disconnected'));
 
 const sessionMiddleware = session({
   secret: process.env.SESSION_SECRET,
@@ -19,6 +23,7 @@ const sessionMiddleware = session({
   saveUninitialized: false,
   store: MongoStore.create({
     mongoUrl: dbUrl,
+    ttl: 24 * 60 * 60, // Optional: 1-day session expiration
   }),
   cookie: {
     maxAge: 1000 * 60 * 60 * 24, // 1 day
