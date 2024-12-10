81e7702f34ec53cef53776a64bf57964a68418de
PriyanshiATSamagra <priyanshi@samagragovernance.in>
2024-12-10 17:33:01 +0530

    storage value changed on connection

--- config/mongoose-connect.js
+++ config/mongoose-connect.js
@@ -1,16 +1,31 @@
-require('dotenv').config();
-const mongoose = require('mongoose')
-const dbgr = require('debug')('development:mongoose-connetion')
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const dbgr = require('debug')('development:mongoose-connection');
 
-const dbUrl = `${process.env.MONGOOSE_CONNECT_URL}/mrbilly` || `${process.env.MONGOOSE_CONNECT_SERVER}/mrbilly`;
+const dbUrl =
+  process.env.MONGOOSE_CONNECT_URL
+    ? `${process.env.MONGOOSE_CONNECT_URL}/mrbilly`
+    : `${process.env.MONGOOSE_CONNECT_SERVER}/mrbilly`;
 
 mongoose
-    .connect(dbUrl)
-    .then(function () {
-        dbgr("Connected")
-    })
-    .catch(function (err) {
-        dbgr(err)
-    })
+  .connect(dbUrl)
+  .then(() => dbgr('Connected to MongoDB'))
+  .catch(err => dbgr('Error connecting to MongoDB:', err));
 
-module.exports = mongoose.connection;
+const sessionMiddleware = session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: dbUrl,
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24, // 1 day
+  },
+});
+
+module.exports = {
+  connection: mongoose.connection,
+  sessionMiddleware,
+};
